d2c10e5465b47b7b3aaadb0e1aea272e9eee5804
mikolaj <m1k0103@therockman.tech>
2025-10-18 18:21:13 +0100

    now fetches song details, finish fetching lyrics

---
+++ soundcloudLyricsFirefox.js
@@ -0,0 +1,45 @@
+function getCurrentSongDetails(){
+    const artist = document.querySelectorAll(".playbackSoundBadge__lightLink.sc-link-light.sc-link-secondary.sc-truncate.sc-text-h5")[0].title
+    const songName = document.querySelectorAll(".playbackSoundBadge__titleLink.sc-truncate.sc-text-h5.sc-link-primary")[0].title
+    //console.log(`${artist} ${songName}`)
+    return [artist,songName]
+}
+
+function getLyrics(artist,songName){
+    console.log(artist, songName)
+    var songNameClean = songName.toLowerCase().replace(/[^a-zA-Z0-9]/g, '').split(" ")[0]
+    var artistClean = artist.toLowerCase().replace(/[^a-zA-Z0-9]/, '').split(" ")[0]
+    console.log(songNameClean,artistClean)
+    
+
+    // fetches lyrics
+    const url = `https://www.azlyrics.com/lyrics/${artistClean}/${songNameClean}.html`
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET", url)
+    xhr.responseType = "document"
+    xhr.send()
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) { // if request is successful
+            const raw_page_contents = xhr.responseText
+            console.log(raw_page_contents)
+        
+        }else{
+            console.log(`Error ${xhr.status}`)
+        }
+    }
+
+}
+
+
+window.onload = function() {
+    console.log("test")
+    var songDetails = getCurrentSongDetails()
+
+    var artist = songDetails[0]
+    var songName = songDetails[1]
+    getLyrics(artist, songName)
+}
+
+
+
+document.getElementsByTagName
