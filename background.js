89919ae5ad3ba85876029353a8a0a2f76dfe8b3f
mikolaj <m1k0103@therockman.tech>
2025-10-22 01:05:10 +0100

    added fucking background file idk why

---
+++ background.js
@@ -0,0 +1,43 @@
+async function getLyrics(artist,songName){
+    console.log(artist, songName)
+    var songNameClean = songName.toLowerCase().replace(/[^a-zA-Z0-9]/g, '').split(" ")[0]
+    var artistClean = artist.toLowerCase().replace(/[^a-zA-Z0-9]/, '').split(" ")[0]
+    console.log(songNameClean,artistClean)
+    
+
+    // fetches lyrics
+    //const xhr = new XMLHttpRequest()
+    //xhr.open("GET", url)
+    //xhr.responseType = "document"
+    //xhr.send()
+    //console.log(xhr)
+    //xhr.onload = () => {
+        //    if (xhr.readyState == 4 && xhr.status == 200) { // if request is successful
+        //        const raw_page_contents = xhr.responseText
+        //        console.log(raw_page_contents)
+        //        
+        //    }else{
+            //        console.log(`Error ${xhr.status}`)
+            //    }
+            //}
+            //console.log(xhr)
+            //console.log("abcdef")
+            //let response = await fetch(url)
+            //console.log(response)
+            //let raw_text = await response.textContent
+            //try {
+    const url = `https://www.azlyrics.com/lyrics/${artistClean}/${songNameClean}.html`
+
+    const response = await fetch(url)
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        const result = await response.text()
+        console.log(result)
+    //} catch (error){
+        //console.error(error.message)
+    //}
+    console.log("test")
+
+}
